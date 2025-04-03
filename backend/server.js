1ed1af4667f41672b0baaeb797a9a593595242a7
Kasidhad <kkasidhad@gmail.com>
2025-04-04 03:44:55 +0700

    port edit

--- backend/server.js
+++ backend/server.js
@@ -28,3 +28,25 @@ app.get('*', (req, res) => {
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
   });
+
+
+  // const express = require('express');
+  // const cors = require('cors');
+  // const connectDB = require('./config/db');
+  // require('dotenv').config();
+  
+  // const app = express();
+  
+  // // Connect Database
+  // connectDB();
+  
+  // // Middleware
+  // app.use(express.json());
+  // app.use(cors());
+  
+  // // Routes
+  // app.use('/api/auth', require('./routes/auth'));
+  
+  // const PORT = process.env.PORT || 5000;
+  
+  // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));  
