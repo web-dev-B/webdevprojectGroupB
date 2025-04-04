7fcc9c6278ff3ffff1d84e747a80477d41c42dfe
Kasidhad <kkasidhad@gmail.com>
2025-04-04 16:53:47 +0700

    corrected port from 5432 to 5435

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -6,7 +6,7 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: 5432,
+  port: 5435,
 });
 
 const connectDB = async () => {
