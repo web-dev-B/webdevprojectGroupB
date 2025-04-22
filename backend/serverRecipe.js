660afe4bb6ba0cf5367fa8d6f053ca829e0b07a3
Van Tam Pham <pvantam2003@gmail.com>
2025-04-22 13:13:11 +0300

    update backend CreateReicpe function

--- backend/serverRecipe.js
+++ backend/serverRecipe.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const postsRouter = require('./src/routes/postsRoutes');
 const ingredientsRouter = require('./src/routes/ingredientsRoutes');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,11 +1,15 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'recipe_website',
-  password: 'root',
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
   port: 5432,
+  ssl: {
+    rejectUnauthorized:false,
+  }
 });
 
 // Connect to PostgreSQL
