46c9c1a96d964bc170ca113442b068df80cbf35e
Kasidhad <kkasidhad@gmail.com>
2025-04-25 17:04:03 +0700

    add profile logic

---
+++ frontend/public/js/profile.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetchUserProfile();
+});
+
+  
+function fetchUserProfile() {
+    const token = localStorage.getItem('token');
+    const base_url = fodweb.postgres.database.azure.com
+    fetch(base_url + '/api/auth/auth', {
+        method: 'GET',
+         headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Failed to fetch user data');
+        }
+        return response.json();
+    })
+    .then(data => {
+        document.getElementById('name').textContent = `Name: ${data.name}`;
+        document.querySelector('.card-text').textContent = `Name: ${data.email}`;
+        document.getElementById('following-count').textContent = data.followingCount;
+  
+        const profileImg = document.querySelector('.card-img-top');
+        if (data.profilePictureUrl) {
+            profileImg.src = data.profilePictureUrl;
+        } else {
+            profileImg.src = '/assets/images/default-avatar.png'; 
+        }
+    })
+
+    .catch(error => {
+        console.error('Error loading profile:', error);
+    });
+}
+  
